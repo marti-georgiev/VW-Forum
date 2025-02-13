17ecf18e9cbfc362d726961a45e345ca477527a2
marti-georgievcls <cim2000@abv.bg>
2025-02-13 15:22:39 +0200

    Entity repos

---
+++ src/VWForum/Data/VWForum.Data/Repositories/VWThreadRepository.cs
@@ -0,0 +1,14 @@
+﻿using VWForum.Data.Models;  
+using Microsoft.AspNetCore.Http;
+using VWForum.Web.Data;
+
+namespace VWForum.Data.Repositories
+{
+    public class VWThread : MetaDataBaseGenericRepository<VWThread>
+    {
+        public VWThread(VWForumDbContext dbcontext, IHttpContextAccessor? httpContextAccessor) 
+            : base(dbcontext, httpContextAccessor)
+        {
+        }
+    }
+}
